09fccc0793e181b14811746fc99f7ac18d148cac
PatrickOndreovici <66522995+PatrickOndreovici@users.noreply.github.com>
2021-12-08 22:30:22 +0200

    v2

--- canvas.js
+++ canvas.js
@@ -1,146 +1,148 @@
-var n = 10, v = [], minimDist = Infinity, p1 = {x:0,y:0}, p2 = {x:0,y:0}, cp1 = {x:0,y:0}, cp2 = {x:0,y:0}, midLine = null;
+let numberOfPlanes = 10
+let p1 = {x:0,y:0}, p2 = {x:0,y:0}
+let  minimDist = Infinity
+let planesPosition = []
+let index = -1
 function preload() {
     img = loadImage('avion.png');
 }
 
 function setup(){
     createCanvas(window.innerWidth, window.innerHeight);
-    for (var i = 0; i < n; ++i){
+    for (var i = 0; i < numberOfPlanes; ++i){
         var x = Math.floor(random(window.innerWidth - 100)) + 50;
         var y = Math.floor(random(window.innerHeight - 100)) + 50;
-        v.push({x:x,y:y});
+        planesPosition.push({x:x,y:y,index:i});
     }
-    v.sort((a, b) => a.x - b.x);
-    minDist(v, 0, n - 1);
+    planesPosition.sort((a, b) => a.x - b.x);
+    minDist(planesPosition, 0, numberOfPlanes - 1);
 }
 
 function draw(){
+    minimDist = Infinity
+    planesPosition.sort((a, b) => a.x - b.x);
+    minDist(planesPosition, 0, numberOfPlanes - 1);
     background(0,191,255);
     strokeWeight(4);
-    stroke("black");
-    if (midLine != null){
-        line(midLine, 0, midLine, height);
-    }
-    line(cp1.x, cp1.y, cp2.x, cp2.y);
     stroke('rgb(0,100,0)');
     line(p1.x, p1.y, p2.x, p2.y);
     strokeWeight(1);
     stroke("black");
-    for (let i = 0; i < n; ++i){
+    move(index)
+    for (let i = 0; i < numberOfPlanes; ++i){
         imageMode(CENTER);
-        image(img, v[i].x, v[i].y, 25, 25);
+        image(img, planesPosition[i].x, planesPosition[i].y, 25, 25);
         textSize(14);
-        text('(' + v[i].x + ',' + v[i].y + ')', v[i].x, v[i].y - 20);
+        text('(' + planesPosition[i].x + ',' + planesPosition[i].y + ')', planesPosition[i].x, planesPosition[i].y - 20);
     }
 }
 
+function mouseClicked() {
+    console.log("DA")
+    if (index != -1){
+        index = -1
+    }
+    else{
+        let dist = Infinity
+        let newIndex = -1
+        for (let i = 0; i < planesPosition.length; ++i){
+            let newDist = calcDist(planesPosition[i], {x: mouseX, y: mouseY})
+            if (newDist < dist){
+                dist = newDist
+                newIndex = planesPosition[i].index
+            }
+        }
+        if (dist <= 100 && newIndex != -1){
+            index = newIndex
+        }
+    }
+  }
 
-async function calcDist(a, b){
-    await sleep(200);
+function calcDist(a, b){
     return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
 }
 
-function resetChecking(){
-    cp1 = {x:0,y:0};
-    cp2 = {x:0,y:0};
-    workingArea = {x1:0,y1:0,x2:0,y2:0};
-    midLine = null;
+function move(index){
+    if (index == -1){
+        return
+    }
+    let pos
+    for (let i = 0; i < planesPosition.length; ++i){
+        if (planesPosition[i].index == index){
+            pos = i 
+            break
+        }
+    }
+    planesPosition[pos].x = mouseX
+    planesPosition[pos].y = mouseY
 }
 
-async function Merge(v, st, dr){
+
+function Merge(v, st, dr){
     var mid = Math.floor((st + dr) / 2);
     var i = st, j = mid + 1;
     var aux = [];
     while (i <= mid && j <= dr){
-        if (v[i].y < v[j].y){
-            aux.push(v[i++]);
+        if (planesPosition[i].y < planesPosition[j].y){
+            aux.push(planesPosition[i++]);
         }
         else{
-            aux.push(v[j++]);
+            aux.push(planesPosition[j++]);
         }
     }
     while (i <= mid){
-        aux.push(v[i++]);
+        aux.push(planesPosition[i++]);
     }
     while (j <= dr){
-        aux.push(v[j++]);
+        aux.push(planesPosition[j++]);
     }
     for (i = st, j = 0; i <= dr; ++i, ++j){
-        v[i] = aux[j];
+        planesPosition[i] = aux[j];
     } 
 }
 
-async function minDist(v, st, dr){
-    if (dr - st + 1 == 3){
-        cp1 = v[st];
-        cp2 = v[st + 1];
-        var dist1 = await calcDist(v[st], v[st + 1]);
-        if (dist1 < minimDist){
-            minimDist = dist1;
-            p1 = v[st];
-            p2 = v[st + 1];
-        }
-        cp1 = v[st];
-        cp2 = v[dr];
-        var dist2 = await calcDist(v[st], v[dr]);
-        if (dist2 < minimDist){
-            minimDist = dist2;
-            p1 = v[st];
-            p2 = v[dr];
-        }
-        cp1 = v[st + 1];
-        cp2 = v[dr];
-        var dist3 = await calcDist(v[st + 1], v[dr]);
-        if (dist3 < minimDist){
-            minimDist = dist3;
-            p1 = v[st + 1];
-            p2 = v[dr];
-        }
-        resetChecking();
-    }
-    else if (dr - st + 1 == 2){
-        cp1 = v[st];
-        cp2 = v[dr];
-        var dist1 = await calcDist(v[st], v[dr]);
-        if (dist1 < minimDist){
-            minimDist = dist1;
-            p1 = v[st];
-            p2 = v[dr];
-        }
-        resetChecking();
+
+function minDist(v, st, dr){
+    if (dr - st + 1 <= 3){
+       for (let i = st; i < dr; ++i){
+           for (let j = i + 1; j <= dr; ++j){
+               let newDist = calcDist(planesPosition[i], planesPosition[j])
+               if (newDist < minimDist){
+                   minimDist = newDist
+                   p1 = planesPosition[i]
+                   p2 = planesPosition[j]
+               }
+           }
+       }
     }
     else{
         var mid = Math.floor((st + dr) / 2);
-        var midX = v[mid].x, stX = v[st].x, drX = v[dr].x;
-        await minDist(v, st, mid);
-        await minDist(v, mid + 1, dr);
+        var midX = planesPosition[mid].x, stX = planesPosition[st].x, drX = planesPosition[dr].x;
+        minDist(planesPosition, st, mid);
+        minDist(planesPosition, mid + 1, dr);
         workingArea = {x1: stX, y1: 0, x2: drX, y2: window.innerHeight};
-        await Merge(v, st, dr);
+        Merge(planesPosition, st, dr);
         midLine = midX;
         var aux = [];
         for (let i = st; i <= dr; ++i){
-            cp1 = v[i];
-            cp2 = {x : midX, y: v[i].y};
-            var d = await calcDist({x : v[i].x, y : 0}, {x : midX, y : 0});
-            if (d < minimDist){
-                aux.push(v[i]);
+            var dist = calcDist({x : planesPosition[i].x, y : 0}, {x : midX, y : 0});
+            if (dist < minimDist){
+                aux.push(planesPosition[i]);
             }
         }
         for (let i = 0; i < aux.length; ++i){
             for (let j = i + 1; j < aux.length && j - i + 1 <= 8; ++j){
-                cp1 = aux[i];
-                cp2 = aux[j];
-                var d = await calcDist(aux[i], aux[j]);
-                if (d < minimDist){
-                    minimDist = d;
+                var newDist = calcDist(aux[i], aux[j]);
+                if (newDist < minimDist){
+                    minimDist = newDist;
                     p1 = aux[i];
                     p2 = aux[j];
                 }
             }
         }
     }
-    resetChecking();
 }
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
